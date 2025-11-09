eb738aff55447b1a2b91cec888dc1033bfb73940
Strange-Quark-007 <masteryasin@ymail.com>
2025-11-09 16:11:47 +0530

    initial commit

---
+++ src/main/java/strangequark/chestfinder/Chestfinder.java
@@ -0,0 +1,15 @@
+package strangequark.chestfinder;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Chestfinder implements ModInitializer {
+    public static final String MOD_ID = "chestfinder";
+
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+    @Override
+    public void onInitialize() {
+    }
+}
